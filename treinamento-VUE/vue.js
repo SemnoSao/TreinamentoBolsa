01b214aded9122af6ea7c95ebea9d1edc9dec044
SemnoSao <arthur.caccavo@gmail.com>
2021-05-19 14:46:22 -0300

    leitura correta das info da API através do app VUE

--- treinamento-VUE/vue.js
+++ treinamento-VUE/vue.js
@@ -1,24 +1,30 @@
-db = [{"id":1, "nome": "O Nome do Vento", "autor": "Patrick Rothfuss"}, 
-        {"id":2, "nome": "A Musica do Silencio", "autor": "Patrick Rothfuss"},
-        {"id":3, "nome": "Deuses Americanos", "autor": "Neil Gaiman"}]
-
 const dbControl = Vue.createApp({
-    setup() {
-        const headers = Object.keys(db[1])
-        const books = db
-        const currentObj = {}
-        for(h in headers){
-            currentObj[h] = ""
-        }
+    data(){
         return {
-            headers,
-            books,
-            currentObj
+            headers: [],
+            books: [],
+            currentObj: []
         }
     },
-    computed:{
-        currentObjisNew(){}
+    mounted() {
+        axios
+            .get('http://localhost/livros')
+            .then(response => (this.books = response.data))
+    },
+    computed: {
+        books: {
+            set(){
+                this.headers = Object.keys(this.books[1]).filter(function(value, index, arr){ 
+                    return (value != "created_at" && value != "updated_at");
+                });
+                this.currentObj = {}
+                for(h in this.headers){
+                    this.currentObj[h] = ""
+                }
+            }
+        }
     }
+
 })
 
 dbControl.mount('#dbControl')
