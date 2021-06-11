084d63a219c8bac7e963af8e11a0579f8f023058
SemnoSao <arthur.caccavo@gmail.com>
2021-06-11 14:27:40 -0300

    ERRO: Vue.mixin is not a function

--- treinamento-VUE/vue.js
+++ treinamento-VUE/vue.js
@@ -1,23 +1,39 @@
+const store = new Vuex.Store({
+    state: {
+        books: []
+    },
+    getters: {
+        headers: state => {
+            return Object.keys(this.books[1]).filter(value => (value != "created_at" && value != "updated_at"));
+        }
+    },
+    mutations: {
+        SET_Item (state, items) {
+            state.books = items
+        }
+    },
+    actions: {
+        loadItems ({ commit }){
+            axios
+                .get('http://localhost/livros')
+                .then(response => commit('SET_Items', items))
+        }
+    }
+});
+
 const dbControl = Vue.createApp({
+    store,
     data(){
         return {
-            headers: [],
-            books: [],
             currentObj: {}
         }
     },
-    mounted() {
-        axios
-            .get('http://localhost/livros')
-            .then(response => (this.books = response.data))
-    },
-    computed: {
-        books: {
-            set(){
-                this.headers = Object.keys(this.books[1]).filter(function(value, index, arr){ 
-                    return (value != "created_at" && value != "updated_at");
-                });
-            }
+    computed:{
+        books() {
+            return this.$store.state.books
+        },
+        headers() {
+            return this.$store.getters.headers
         }
     },
     methods: {
@@ -28,12 +44,12 @@ const dbControl = Vue.createApp({
                     nome: this.currentObj['nome'],
                     autor: this.currentObj['autor']
                 })
-                .then(response => (this.books = response.data))
+                .then(response => (this.$store.commit('SET_Items', items)))
         },
         destroy: function(){
             axios
                 .post('http://localhost/livro/'+this.currentObj['id']+'/excluir')
-                .then(response => (this.books = response.data))
+                .then(response => (this.$store.commit('SET_Items', items)))
         },
         update: function(){
             axios
@@ -42,13 +58,12 @@ const dbControl = Vue.createApp({
                    nome: this.currentObj['nome'],
                    autor: this.currentObj['autor']
                 })
-                .then(response => (this.books = response.data))
+                .then(response => (this.$store.commit('SET_Items', items)))
         },
         link: function(event){
-            for (i=0;i<this.headers.length;i++) this.currentObj[this.headers[i]] = event.target.parentElement.cells[i].innerHTML;
+            for (i=0;i<this.$store.getters.headers.length;i++) this.currentObj[this.$store.getters.headers[i]] = event.target.parentElement.cells[i].innerHTML;
         }
     }
-
 })
 
 dbControl.mount('#dbControl')
